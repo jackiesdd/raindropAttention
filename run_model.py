63ef2ba1ff3d41f11ce890bc34ef5c849d058760
jackiesdd <46415321+jackiesdd@users.noreply.github.com>
2020-11-02 10:10:29 +0800

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -14,11 +14,11 @@ from models import model
 def parse_args():
 	parser = argparse.ArgumentParser(description='rainDrop arguments')
 	# train params
-	parser.add_argument('--phase', type=str, default='test', help='"test" mode')
+	parser.add_argument('--phase', type=str, default='test', help='determine whether "train" or "test" ')
 	parser.add_argument('--datalist', type=str, default='./raindrop.txt', help='training datalist')
 	parser.add_argument('--tfrecord_dir', type=str, default='./tf/train2.tfrecords', help='the location of tfrecord')
 	parser.add_argument('--batch_size', type=int, default=10, help='training batch size')
-	parser.add_argument('--epoch', type=int, default= 4000, help='training epoch number')
+	parser.add_argument('--epoch', type=int, default= 4000, help='training epoch number,1000 for 4 times data augmentation')
 	parser.add_argument('--learning_rate', type=float, default=1e-4, help='initial learning rate')
 	parser.add_argument('--gpu', dest='gpu_id', type=str, default='0', help='use gpu or cpu')
 	parser.add_argument('--modelname', type=str, default='rainDrop', help='model type')
@@ -43,5 +43,8 @@ def main(_):
 	if args.phase == 'test':
 		rainDrop = model.RainDropRemoval(args)
 		rainDrop.test(args.inputdata_path,args.output_path)
+	elif args.phase == 'train':
+		rainDrop = model.RainDropRemoval(args)
+		rainDrop.train()
 if __name__ == '__main__':
 	tf.app.run()
